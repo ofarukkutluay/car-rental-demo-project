69cb98ab6a8c048dd88392730159991d3598c7bb
dRcmd <76197823+dRcmd@users.noreply.github.com>
2021-04-10 16:01:39 +0300

    ufak bi düzeltme

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!:FormGroup;
+  
+  constructor(private formBuilder:FormBuilder,
+     private authService:AuthService,private localStorageService:LocalStorageService,
+      private toastrService:ToastrService, private router:Router,
+      private userService: UserService) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
   }
 
+  createRegisterForm(){
+    this.registerForm = this.formBuilder.group({
+      firstName:["",Validators.required],
+      lastName:["",Validators.required],
+      email: ["",Validators.required],
+      password:["",Validators.required]
+    })
+  }
+
+  register(){
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value);
+      let registerModel = Object.assign({},this.registerForm.value)
+
+      this.authService.login(registerModel).subscribe(response=>{
+        this.toastrService.info(response.message)
+        this.localStorageService.setToken(response.data.token)
+        localStorage.setItem('email',registerModel.email.toString())
+        this.router.navigate([""])
+      },responseError=>{
+        //console.log(responseError)
+        this.toastrService.error(responseError.error)
+      })
+    }
+  }
 }
